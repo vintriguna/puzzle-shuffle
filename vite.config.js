9024930bca633e4a47eafd678f12945dbaef71bd
Vinayak Trigunayat <vinayaktrigunayat11@gmail.com>
2024-07-16 19:49:29 +0530

    deploy workflows

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shuffle-puzzle/",
   plugins: [react()],
+  base: "/shuffle-puzzle/",
 });
